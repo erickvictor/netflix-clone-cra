ad7b3f728af9aa338f7c4d7be991db181e389e34
Erick Bernardo <erickvictor@gmail.com>
2021-11-16 21:00:35 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import Logo from './images/logo.png'
+import Motivo1 from './images/motivo-1.png'
+import Motivo2 from './images/motivo-2.png'
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div>
+          <img src={Logo} alt="Logo" />
+        </div>
+        <div id="login">
+          <select id="idioma">
+            <option value="PT">Português</option>
+            <option value="ING">Ingles</option>
+          </select>
+          <button>Entrar</button>
+        </div>
       </header>
-    </div>
+      <section id="banner">
+        <h1>
+          Filmes, séries e muito <br />
+          mais. Sem limites.
+        </h1>
+        <h3>Assista onde quiser. Cancele quando quiser.</h3>
+        <small>
+          Pronto para assistir? Informe seu email para criar ou reiniciar sua
+          assinatura.
+        </small>
+        <div>
+          <input type="email" placeholder="Email" />
+          <button>Vamos Lá</button>
+        </div>
+      </section>
+      <hr className="divisor" />
+      <section className="motivos">
+        <div>
+          <h1>Aproveite na TV.</h1>
+          <p>
+            Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV,
+            aparelhos de Blu-ray e outros dispositivos.
+          </p>
+        </div>
+        <div>
+          <img src={Motivo1} alt="Motivo 1" />
+        </div>
+      </section>
+      <hr className="divisor" />
+      <section className="motivos alterna">
+        <div>
+          <h1>Aproveite na TV.</h1>
+          <p>
+            Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV,
+            aparelhos de Blu-ray e outros dispositivos.
+          </p>
+        </div>
+        <div>
+          <img src={Motivo2} alt="Motivo 1" />
+        </div>
+      </section>
+      <hr className="divisor" />
+      <section className="motivos">
+        <div>
+          <h1>Aproveite na TV.</h1>
+          <p>
+            Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV,
+            aparelhos de Blu-ray e outros dispositivos.
+          </p>
+        </div>
+        <div>
+          <img src={Motivo1} alt="Motivo 1" />
+        </div>
+      </section>
+      <hr className="divisor" />
+      <section className="motivos alterna">
+        <div>
+          <h1>Aproveite na TV.</h1>
+          <p>
+            Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV,
+            aparelhos de Blu-ray e outros dispositivos.
+          </p>
+        </div>
+        <div>
+          <img src={Motivo1} alt="Motivo 1" />
+        </div>
+      </section>
+      <hr className="divisor" />
+    </>
   );
 }
 
